a69ebba886e854d97e047772bfeeb8e2428f1f6c
oronm18 <oronm18@gmail.com>
2023-07-07 10:40:17 +0300

    Commit utils file.

---
+++ budget-app/src/Utils.tsx
@@ -0,0 +1,20 @@
+export interface Field {
+    id: string;
+    display_name: string;
+    type: string;
+    choices?: string[];
+    default: any;
+  }
+
+  export function createDynamicType(fields: Field[]): Record<string, any> {
+    const typeKeys = fields.reduce((keys, field) => {
+      keys[field.id] = field.default;
+      return keys;
+    }, {} as Record<string, any>);
+  
+    return typeKeys;
+  }
+
+  export const handleRefreshNavigate = (path: string) => {
+    window.location.href = path; // Navigate and refresh the page
+  };
